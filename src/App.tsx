1f7484cdbd254257e47757f5e5f7558b71cfb3d9
Bilal Ezzaim <bilalzaim499@gmail.com>
2025-02-27 22:29:12 +0000

    Fix Git config issue

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,7 @@ export function App() {
   }
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/moride-front-end">
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
