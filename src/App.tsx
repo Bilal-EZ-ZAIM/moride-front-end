5bf94e0fe6267ed07ecc499892cba6f3c39526a2
Bilal Ezzaim <bilalzaim499@gmail.com>
2025-03-04 14:22:46 +0000

    Création d'un guard pour sécuriser l'accès aux pages privées

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,7 @@ import CreateDiriverProfile from "./pages/CreateDriverProfile";
 import { DriverProfile } from "./pages/DriverProfile";
 import { DriverDetails } from "./pages/DriverDetails";
 import MyTrips from "./pages/MyTrips";
+import { ProtectedRoute } from "./guard/PrivateRoute";
 
 export function App() {
   const dispatch = useAppDispatch();
@@ -41,25 +42,31 @@ export function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="drivers" element={<DriversListing />} />
-          <Route path="/MyTrips" element={<MyTrips />} />
-          <Route path="/tripDetails/:id" element={<TripDetails />} />
-          <Route path="bookings" element={<TripOffers />} />
+
+          <Route element={<ProtectedRoute />}>
+            <Route path="/payment" element={<Payment />} />
+            <Route path="/MyTrips" element={<MyTrips />} />
+            <Route path="/tripDetails/:id" element={<TripDetails />} />
+            <Route path="bookings" element={<TripOffers />} />
+            <Route path="driver" element={<DriverProfile />} />
+            <Route path="driver/dashboard" element={<DriverDashboard />} />
+            <Route path="/profile" element={<UserProfile />} />
+            <Route path="/change-password" element={<ChangePassword />} />
+            <Route path="/welcome/page" element={<WelcomePage />} />
+            <Route path="/create/profile" element={<CreateProfile />} />
+            <Route path="/create/driver" element={<CreateDiriverProfile />} />
+          </Route>
           <Route path="bookTrip" element={<BookTrip />} />
-          <Route path="driver" element={<DriverProfile />} />
           <Route path="/driver/:id" element={<DriverDetails />} />
-          <Route path="driver/dashboard" element={<DriverDashboard />} />
-          <Route path="/profile" element={<UserProfile />} />
-          <Route path="/payment" element={<Payment />} />
-          <Route path="/change-password" element={<ChangePassword />} />
-          <Route path="/welcome/page" element={<WelcomePage />} />
-          <Route path="/create/profile" element={<CreateProfile />} />
-          <Route path="/create/driver" element={<CreateDiriverProfile />} />
+          <Route path="drivers" element={<DriversListing />} />
         </Route>
-        <Route path="/reset-password" element={<ResetPassword />} />
-        <Route path="/messages" element={<Messages />} />
-        <Route path="/admin" element={<SuperAdminDashboard />} />
-        <Route path="/admin/users" element={<AdminUsers />} />
+        <Route element={<ProtectedRoute />}>
+          <Route path="/reset-password" element={<ResetPassword />} />
+          <Route path="/messages" element={<Messages />} />
+          <Route path="/admin" element={<SuperAdminDashboard />} />
+          <Route path="/admin/users" element={<AdminUsers />} />
+        </Route>
+
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
---
+++ src/guard/PrivateRoute.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Navigate, Outlet } from "react-router-dom";
+import { useAppSelector } from "../hooks";
+
+interface ProtectedRouteProps {
+  redirectPath?: string;
+  children?: React.ReactNode;
+}
+
+export const ProtectedRoute: React.FC<ProtectedRouteProps> = ({
+  redirectPath = "/login",
+  children,
+}) => {
+  const { isLogin } = useAppSelector((state) => state.auth);
+  if (!isLogin) {
+    return <Navigate to={redirectPath} replace />;
+  }
+
+  return children ? <>{children}</> : <Outlet />;
+};
